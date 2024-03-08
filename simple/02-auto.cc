2d93c26544ea5a23fa1f35a51802dbff9388ea1c
Lionel Auroux <lionel.auroux@gmail.com>
2024-03-08 12:17:59 +0100

    WIP

---
+++ simple/02-auto.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cxxabi.h>
+
+using namespace std;
+
+const char *demangle(const char *ty)
+{
+	int status;
+	return abi::__cxa_demangle(ty, 0, 0, &status);
+}
+
+int main()
+{
+	auto i = 42;
+	auto txt = "coucou\n";
+
+	cout << "i = " << i << endl;
+	cout << "type i = " << demangle(typeid(i).name()) << endl;
+	cout << "type txt = " << demangle(typeid(txt).name()) << endl;
+	return 0;
+}
