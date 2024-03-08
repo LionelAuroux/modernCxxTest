e1ca010a420c74020e175fb08616cdcc0df4477e
Lionel Auroux <lionel.auroux@gmail.com>
2024-03-08 11:51:37 +0100

    Initial code

---
+++ simple/00-cout.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "This ";
+       	cout << toupper("his");
+       	cout << " the old ";
+       	cout << tolower("WAY");
+       	cout << " to print things." << endl;
+	return 0;
+}
---
+++ simple/01-print.cc
@@ -0,0 +1,9 @@
+#include <print>
+
+using namespace std;
+
+int main(void)
+{
+	println("This {} a new {} to print things.", "is", "way");
+	return 0;
+}
